8d161671f212177d5d64c68aaad5c08854d09a22
Vladimir Sentiurin <sentiurin@Vladimirs-MacBook-Pro.local>
2021-03-30 00:11:50 +0300

    debug

--- adapters/deno.ts
+++ adapters/deno.ts
@@ -1,11 +1,13 @@
 type Modules = { [key: string]: any };
 
 function readFiles(path: string): Promise<string[]> {
+  console.debug(path);
+  
   return new Promise(async (resolve, reject) => {
     const paths: string[] = [];
 
     try {
-      for await (const entry of Deno.readDirSync('.')) {
+      for await (const entry of Deno.readDirSync(path)) {
         if (entry.isFile) {
           paths.push(entry.name);
         }
@@ -13,6 +15,8 @@ function readFiles(path: string): Promise<string[]> {
     } catch (exception) {
       reject(exception);
     }
+
+    console.debug(paths);
 
     resolve(paths);
   });
@@ -35,6 +39,8 @@ async function exportAll(path: string): Promise<Modules> {
       } catch (exception) {}
     })
   );
+
+  console.debug(modules);
 
   return modules;
 }
