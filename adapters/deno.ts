21d4fef3720c44f747bce660001094f9f179a8c1
Vladimir Sentiurin <sentiurin@Vladimirs-MacBook-Pro.local>
2021-03-30 00:07:21 +0300

    add adapter for deno support

---
+++ adapters/deno.ts
@@ -0,0 +1,42 @@
+type Modules = { [key: string]: any };
+
+function readFiles(path: string): Promise<string[]> {
+  return new Promise(async (resolve, reject) => {
+    const paths: string[] = [];
+
+    try {
+      for await (const entry of Deno.readDirSync('.')) {
+        if (entry.isFile) {
+          paths.push(entry.name);
+        }
+      }
+    } catch (exception) {
+      reject(exception);
+    }
+
+    resolve(paths);
+  });
+}
+
+async function exportAll(path: string): Promise<Modules> {
+  const modules: any = {};
+  const paths = await readFiles(path);
+
+  const jsFiles = paths.filter((fileName: string): boolean => {
+    return !fileName.startsWith(".") && fileName.endsWith(".js");
+  });
+
+  await Promise.all(
+    jsFiles.map(async (fileName) => {
+      const name = fileName.split('.js')[0];
+      
+      try {
+        modules[name] = (await import(`../${fileName}`)).default;
+      } catch (exception) {}
+    })
+  );
+
+  return modules;
+}
+
+export default exportAll(".");
